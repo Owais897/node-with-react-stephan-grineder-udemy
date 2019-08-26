e692d98b7ccf390114eb5a22cbf4495909d1eb30
Muhammad Owais <m.owais2118@gmail.com>
2019-08-26 13:58:17 +0500

    there where

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = new express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "there", by: "where" });
 });
 
 const PORT = process.env.PORT || 5000;
