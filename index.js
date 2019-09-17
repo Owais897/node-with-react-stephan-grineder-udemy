6afee53d7df10e860e1e675638d66f9a569126a0
Muhammad Owais <m.owais2118@gmail.com>
2019-09-17 19:15:27 +0500

    adding billing and client side app with ignoring sidterm

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
+/////////
+const http = require("http");
+
+process
+  .on("SIGTERM", shutdown("SIGTERM"))
+  .on("SIGINT", shutdown("SIGINT"))
+  .on("uncaughtException", shutdown("uncaughtException"));
+
+setInterval(console.log.bind(console, "tick"), 1000);
+http
+  .createServer((req, res) => res.end("hi"))
+  .listen(process.env.PORT || 3000, () => console.log("Listening"));
+
+function shutdown(signal) {
+  return err => {
+    console.log(`${signal}...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log("...waited 5s, exiting.");
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
+////////
 const b = keys.mongoURI;
 const a =
   " mongodb+srv://owaisprod:owais123@cluster0-cslof.mongodb.net/test?retryWrites=true&w=majority";
